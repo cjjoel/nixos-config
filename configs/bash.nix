bc27771862b88e5860ff23052d7b8648f919a9bf
cjjoel <cjjoelx42@gmail.com>
2020-06-20 15:20:26 +0530

    Remove path alias

--- configs/bash.nix
+++ configs/bash.nix
@@ -4,7 +4,6 @@
     download = "youtube-dl -f bestaudio -x --embed-thumbnail --add-metadata";
     cp = "cp -i";
     df = "df -H";
-    path = "echo -e ${PATH//:/\\n}";
     today = "date +%d-%m-%Y";
   };
 }
