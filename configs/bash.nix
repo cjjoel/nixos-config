d6084c08d2a067c6b170cde6e63a51cd2f4860de
cjjoel <cjjoelx42@gmail.com>
2020-06-20 15:10:17 +0530

    Add bash aliases

--- configs/bash.nix
+++ configs/bash.nix
@@ -1,3 +1,9 @@
 {
   enable = true;
+  shellAliases = {
+    download = "youtube-dl -f bestaudio -x --embed-thumbnail --add-metadata";
+    cp = "cp -i";
+    df = "df -H";
+    path = "echo -e ${PATH//:/\\n}";
+    today = "date +%d-%m-%Y";
 }
