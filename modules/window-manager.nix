0e60eccaa4f431c2aad69675b57c40e8dcab5cc7
cjjoel <cjjoelx42@gmail.com>
2020-06-20 11:27:44 +0530

    inherited lib

--- modules/window-manager.nix
+++ modules/window-manager.nix
@@ -1,12 +1,12 @@
 { config, options, lib, ... }:
 
-with lib; {
+with lib; with types; {
   options.modules.window-manager = {
-      i3wm.enable = mkOption { type = types.bool; default = false; };
+      i3wm.enable = mkOption { type = bool; default = false; };
     };
 
   config.my.home.xsession.windowManager = {
     i3 = mkIf config.modules.window-manager.i3wm.enable
-      (import ../configs/i3wm.nix { lib = lib; });
+      (import ../configs/i3wm.nix { inherit lib; });
   };
 }
