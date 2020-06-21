f865e475d53e5238c90a5a8e1e4e677a2fcc250e
cjjoel <cjjoelx42@gmail.com>
2020-06-21 18:12:01 +0530

    Add zathua configs

---
+++ configs/zathura.nix
@@ -0,0 +1,10 @@
+{
+  enable = true;
+  extraConfig = ''
+    set selection-clipboard clipboard
+    set show-scrollbars true
+    set page-padding 1
+    set statusbar-h-padding 0
+    set statusbar-v-padding 0
+  '';
+}
