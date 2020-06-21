a7f4c01c29745fee73cc51fe0297d25f9daebfdd
cjjoel <cjjoelx42@gmail.com>
2020-06-21 18:17:03 +0530

    Add more modules

---
+++ modules/dev.nix
@@ -0,0 +1,11 @@
+{ config, options, lib, ... }:
+
+with lib; with types; {
+  options.modules.dev = {
+    racket.enable = mkOption { type = types.bool; default = false; };
+  };
+
+  config.my.packages = with pkgs; [
+    (mkIf config.modules.dev.racket.enable racket) 
+  ];
+}
--- modules/utils.nix
+++ modules/utils.nix
@@ -9,20 +9,22 @@ with lib; with types; {
       mpv.enable = mkOption { type = bool; default = false; };
       youtube-dl.enable = mkOption { type = bool; default = false; };
       fd.enable = mkOption { type = bool; default = false; };
+      thefuck.enable = mkOption { type = bool; default = false; };
     };
 
   config = {
     my.packages = with pkgs; [
       (mkIf config.modules.utils.feh.enable feh) 
       (mkIf config.modules.utils.tree.enable tree) 
-      (mkIf config.modules.utils.zathura.enable zathura) 
       (mkIf config.modules.utils.mpv.enable mpv) 
-      (mkIf config.modules.utils.mpv.enable youtube-dl) 
-      (mkIf config.modules.utils.mpv.enable fd) 
+      (mkIf config.modules.utils.youtube-dl.enable youtube-dl) 
+      (mkIf config.modules.utils.fd.enable fd) 
+      (mkIf config.modules.utils.thefuck.enable fd) 
     ];
 
     my.home.programs = {
       git = (mkIf config.modules.utils.git.enable (import ../configs/git.nix));
+      zathura = (mkIf config.modules.utils.zathura.enable (import ../configs/zathura.nix));
     };
   };
 
