6edcc0537331316a0daf38283cc79deb38b65028
cjjoel <cjjoelx42@gmail.com>
2020-06-22 23:04:10 +0530

    Add module dev.heroku

--- modules/dev.nix
+++ modules/dev.nix
@@ -3,9 +3,11 @@
 with lib; with types; {
   options.modules.dev = {
     racket.enable = mkOption { type = types.bool; default = false; };
+    heroku.enable = mkOption { type = types.bool; default = false; };
   };
 
   config.my.packages = with pkgs; [
     (mkIf config.modules.dev.racket.enable racket) 
+    (mkIf config.modules.dev.heroku.enable heroku) 
   ];
 }
