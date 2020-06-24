eae5f7e38b2a042a7e60c42b1e88a7a00b9b406e
cjjoel <cjjoelx42@gmail.com>
2020-06-24 13:15:26 +0530

    Add modules dev.clang and dev.gcc

--- modules/dev.nix
+++ modules/dev.nix
@@ -4,10 +4,15 @@ with lib; with types; {
   options.modules.dev = {
     racket.enable = mkOption { type = types.bool; default = false; };
     heroku.enable = mkOption { type = types.bool; default = false; };
+    clang.enable = mkOption { type = types.bool; default = false; };
+    gcc.enable = mkOption { type = types.bool; default = false; };
   };
 
   config.my.packages = with pkgs; [
     (mkIf config.modules.dev.racket.enable racket) 
     (mkIf config.modules.dev.heroku.enable heroku) 
+    (mkIf config.modules.dev.heroku.enable clang) 
+    (mkIf config.modules.dev.clang.enable clang) 
+    (mkIf config.modules.dev.gcc.enable gcc) 
   ];
 }
