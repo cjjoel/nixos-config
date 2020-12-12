e0e6c46b6de65bcca87dbe1f4ef4b1b9fb2deb86
Joel <cjjoelx42@gmail.com>
2020-12-12 13:03:02 +0530

    Add more programs

--- users/joel.nix
+++ users/joel.nix
@@ -1,35 +1,58 @@
 { pkgs, ... }:
 
-{
+
+let unstable = import <nixos-unstable> { config = { allowUnfree = true; }; }; in {
   imports = [ 
     ./common.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-
-# Devolopment 
-    neovim
-    git
-    sbcl
-    rlwrap
-    gcc
-    python3
-
-# Social
-    discord
-    element-desktop
-    tdesktop
-    zoom-us
-
-# Other
-    pavucontrol
-    firefox
-    neofetch
-    chromium
-    obsidian
-    tree
-    fd
-  ];
+  environment.systemPackages = with pkgs; [ 
+  # Devolopment 
+      neovim
+      git
+      sbcl
+      rlwrap
+      gcc
+      python3
+      python38Packages.pynvim
+      python38Packages.jedi
+      nodejs
+      yarn
+      clang
+      clang-tools
+      cpplint
+      gnumake
+      emacs
+  
+  # Social
+      discord
+      element-desktop
+      tdesktop
+      zoom-us
+  
+  # Other
+      pavucontrol
+      firefox
+      neofetch
+      chromium
+      obsidian
+      tree
+      fd
+      stow
+      mpv
+      vlc
+      youtube-dl
+      sqlite
+      neuron-notes
+      zettlr
+      pandoc
+      haskellPackages.pandoc-citeproc
+      texlive.combined.scheme-basic
+      tectonic
+      zotero
+      xdotool
+      xautomation
+    ];
 
   users.users.joel = {
     isNormalUser = true;
