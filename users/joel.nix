5e622a340fb8edcaef16416a070dfcde7150b7f4
Joel <cjjoelx42@gmail.com>
2021-02-01 14:22:14 +0530

    Remove packages

--- users/joel.nix
+++ users/joel.nix
@@ -6,57 +6,9 @@ let unstable = import <nixos-unstable> { config = { allowUnfree = true; }; }; in
     ./common.nix
   ];
 
-  environment.systemPackages = with pkgs; [ 
-  # Devolopment 
-      neovim
-      git
-      sbcl
-      rlwrap
-      gcc
-      python3
-      python38Packages.pynvim
-      python38Packages.jedi
-      nodejs
-      yarn
-      clang
-      clang-tools
-      cpplint
-      gnumake
-      emacs
-  
-  # Social
-      discord
-      element-desktop
-      tdesktop
-      zoom-us
-  
-  # Other
-      pavucontrol
-      firefox
-      neofetch
-      chromium
-      obsidian
-      tree
-      fd
-      stow
-      mpv
-      vlc
-      youtube-dl
-      sqlite
-      neuron-notes
-      zettlr
-      pandoc
-      haskellPackages.pandoc-citeproc
-      texlive.combined.scheme-basic
-      tectonic
-      zotero
-      xdotool
-      xautomation
-    ];
-
   users.users.joel = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "audio" "video" "networkmanager" ];
+    extraGroups = ["wheel" "audio" "video"];
   };
 
 }
