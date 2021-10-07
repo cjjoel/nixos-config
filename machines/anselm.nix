e6d41d1158015683c601bb5657e259e6256f7722
Joel <cjjoelx42@gmail.com>
2021-10-08 01:35:06 +0530

    Add timer for low battery notification

--- machines/anselm.nix
+++ machines/anselm.nix
@@ -77,38 +77,38 @@
 # battery-notification service is based on domenkozar's suspend.nix
 # https://gist.github.com/domenkozar/82886ee82efee623cdc0d19eb81c7fb7
 
-#  systemd.user.timers."battery-notification" = {
-#    description = "check battery level";
-#    timerConfig.OnBootSec = "1m";
-#    timerConfig.OnUnitInactiveSec = "1m";
-#    timerConfig.Unit = "battery-notification.service";
-#    wantedBy = ["timers.target"];
-#  };
-#
-#  systemd.user.services."battery-notification" = {
-#    description = "battery level notification service";
-#    serviceConfig.PassEnvironment = "DISPLAY";
-#    script = '' 
-#      export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT1/capacity)
-#      export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT1/status)
-#      export warning_level=20
-#      export hibernate_level=10
-#
-#      if [[ $battery_capacity -le $warning_level && $battery_status = "Discharging" ]]; then
-#          ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "LOW BATTERY"
-#      fi
-#
-#      if [[ $battery_capacity -le $hibernate_level && $battery_status = "Discharging" ]]; then
-#          ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery level critical\nComputer will hibernate in 60 seconds."
-#          sleep 60s
-#          battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT1/status)
-#          if [[ $battery_status = "Discharging" ]]; then
-#              systemctl hibernate
-#          fi
-#      fi
-#      '';
-#  };
-#
+systemd.user.timers."battery-notification" = {
+  description = "check battery level";
+  timerConfig.OnBootSec = "1m";
+  timerConfig.OnUnitInactiveSec = "1m";
+  timerConfig.Unit = "battery-notification.service";
+  wantedBy = ["timers.target"];
+};
+
+systemd.user.services."battery-notification" = {
+  description = "battery level notification service";
+  serviceConfig.PassEnvironment = "DISPLAY";
+  script = ''
+    battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT1/capacity)
+    battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT1/status)
+    warning_level=20
+    shutdown_level=10
+
+    if [[ $battery_capacity -le $warning_level && $battery_status = "Discharging" ]]; then
+        ${pkgs.libnotify}/bin/notify-send --urgency=critical "LOW BATTERY"
+    fi
+
+    if [[ $battery_capacity -le $shutdown_level && $battery_status = "Discharging" ]]; then
+        ${pkgs.libnotify}/bin/notify-send --urgency=critical "Battery level critical\nComputer will shutdown in 60 seconds."
+        sleep 60s
+        battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT1/status)
+        if [[ $battery_status = "Discharging" ]]; then
+            systemctl poweroff
+        fi
+    fi
+  '';
+};
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
